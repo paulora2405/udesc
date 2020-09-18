6d20a799ee1e04fb9d7e82890839a1a14001cb3a
paulora2405 <paulora2405@gmail.com>
2020-09-18 17:17:57 -0300

    SOP - Trab 4 - questao3.c DONE

--- sop/trab4/questao3.c
+++ sop/trab4/questao3.c
@@ -1,90 +1,202 @@
 #include <errno.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-extern int errno;
-
-typedef struct {
+typedef struct Requisicao {
   int blocoInicial;
   int numDeBlocos;
   char operacao;
 } Requisicao;
 
+extern int errno;
+Requisicao *req;
+int qntReq = 0;
+int fundiu;
+
+void removeReqDuplicada();
+
 int compare(const void *a, const void *b) { return (*(int *)a - *(int *)b); }
-// qsort(requisicoes, qntReq, sizeof(Requisicao), compare);
 
-int escalonador(Requisicao in, Requisicao requisicoes[], int qntReq) {
-  while(fundirReq(in, requisicoes, qntReq) != qntReq) {}
+/*---------------------------------------------------------------*/
+void fundirR(Requisicao in) {
+  // removeReqDuplicada();
 
-  // requisicoes = realloc(requisicoes, sizeof(Requisicao) * qntReq);
-  // if(requisicoes == NULL) { return -1; }
-  requisicoes[qntReq - 1] = in;
-  return qntReq;
-}
+  int diferencaDeBlocos;
+  int inBlocoFinal = in.blocoInicial + in.numDeBlocos;
+  int reqBlocoFinal;
 
-int fundirReq(Requisicao in, Requisicao req[], int qntReq) {
-  int sobreposicaoDeBlocos;
-  for(int i = 0; i < qntReq - 1; i++) {
-    if(in.operacao == req[i].operacao && in.operacao == 'r') {
-      if(in.blocoInicial < req[i].blocoInicial) {
-        sobreposicaoDeBlocos = in.blocoInicial + in.numDeBlocos - req[i].blocoInicial;
-        if(sobreposicaoDeBlocos >= 0) {
-          int diferencaDeBlocos = req[i].blocoInicial - in.blocoInicial;
-          req[i].blocoInicial = in.blocoInicial;
+  for(int i = 0; i < qntReq; i++) {
+    //
 
-          if(req[i].numDeBlocos + diferencaDeBlocos >= in.numDeBlocos)
+    if(in.operacao != req[i].operacao) continue;
+
+    if(in.blocoInicial == req[i].blocoInicial && in.numDeBlocos == req[i].numDeBlocos) return;
+
+    reqBlocoFinal = req[i].blocoInicial + req[i].numDeBlocos;
+    diferencaDeBlocos = abs(in.blocoInicial - req[i].blocoInicial);
+
+    if(in.blocoInicial < req[i].blocoInicial) {
+      //
+      if(inBlocoFinal >= req[i].blocoInicial) {
+        req[i].blocoInicial = in.blocoInicial;
+
+        if(req[i].numDeBlocos + diferencaDeBlocos >= in.numDeBlocos) {
+          if(req[i].numDeBlocos + diferencaDeBlocos <= 64)
             req[i].numDeBlocos = req[i].numDeBlocos + diferencaDeBlocos;
           else
-            req[i].numDeBlocos = in.numDeBlocos;
+            return;
+        } else
+          req[i].numDeBlocos = in.numDeBlocos;
 
-          return qntReq - 1;
-        }
-      } else if(in.blocoInicial >= req[i].blocoInicial) {
-        sobreposicaoDeBlocos = req[i].blocoInicial + req[i].numDeBlocos - in.blocoInicial;
-        if(sobreposicaoDeBlocos >= 0) {
-          int diferencaDeBlocos = in.blocoInicial - req[i].blocoInicial;
-          req[i].blocoInicial = req[i].blocoInicial;
+        fundiu = 1;
+        fundirR(req[i]);
+      }
+    } else if(in.blocoInicial >= req[i].blocoInicial) {
+      //
+      if(reqBlocoFinal >= in.blocoInicial) {
+        req[i].blocoInicial = req[i].blocoInicial;
 
-          if(req[i].numDeBlocos >= in.numDeBlocos + diferencaDeBlocos)
-            req[i].numDeBlocos = req[i].numDeBlocos;
-          else
+        if(in.numDeBlocos + diferencaDeBlocos >= req[i].numDeBlocos) {
+          if(in.numDeBlocos + diferencaDeBlocos <= 64)
             req[i].numDeBlocos = in.numDeBlocos + diferencaDeBlocos;
+          else
+            return;
 
-          return qntReq - 1;
-        }
+        } else
+          req[i].numDeBlocos = req[i].numDeBlocos;
+
+        fundiu = 1;
+        fundirR(req[i]);
       }
     }
   }
-  return qntReq;
+  return;
 }
 
-void removeReqDuplicada(Requisicao req[], int qntReq) {}
+/*---------------------------------------------------------------*/
+void fundirW(Requisicao in) {
+  int diferencaDeBlocos;
+  int inBlocoFinal = in.blocoInicial + in.numDeBlocos;
+  int reqBlocoFinal;
 
+  for(int i = 0; i < qntReq; i++) {
+    //
+
+    if(in.operacao != req[i].operacao) continue;
+
+    if(in.blocoInicial == req[i].blocoInicial && in.numDeBlocos == req[i].numDeBlocos) return;
+
+    reqBlocoFinal = req[i].blocoInicial + req[i].numDeBlocos;
+    diferencaDeBlocos = abs(in.blocoInicial - req[i].blocoInicial);
+
+    if(in.blocoInicial < req[i].blocoInicial) {
+      //
+      if(inBlocoFinal == req[i].blocoInicial) {
+        req[i].blocoInicial = in.blocoInicial;
+
+        if(req[i].numDeBlocos + diferencaDeBlocos >= in.numDeBlocos) {
+          if(req[i].numDeBlocos + diferencaDeBlocos <= 64)
+            req[i].numDeBlocos = req[i].numDeBlocos + diferencaDeBlocos;
+          else
+            return;
+        } else
+          req[i].numDeBlocos = in.numDeBlocos;
+
+        fundiu = 1;
+        fundirR(req[i]);
+      }
+    } else if(in.blocoInicial >= req[i].blocoInicial) {
+      //
+      if(reqBlocoFinal == in.blocoInicial) {
+        req[i].blocoInicial = req[i].blocoInicial;
+
+        if(in.numDeBlocos + diferencaDeBlocos >= req[i].numDeBlocos) {
+          if(in.numDeBlocos + diferencaDeBlocos <= 64)
+            req[i].numDeBlocos = in.numDeBlocos + diferencaDeBlocos;
+          else
+            return;
+
+        } else
+          req[i].numDeBlocos = req[i].numDeBlocos;
+
+        fundiu = 1;
+        fundirR(req[i]);
+      }
+    }
+  }
+  return;
+}
+
+/*---------------------------------------------------------------*/
+void escalonador(Requisicao in) {
+  fundiu = 0;
+  if(in.operacao == 'w') {
+    fundirW(in);
+  } else
+    fundirR(in);
+  if(fundiu == 0) {
+    req = realloc(req, sizeof(Requisicao) * ++qntReq);
+    if(req == NULL) {
+      free(req);
+      perror("Erro na realocação de memória");
+      exit(EXIT_FAILURE);
+    }
+    req[qntReq - 1] = in;
+  }
+  removeReqDuplicada();
+}
+
+/*---------------------------------------------------------------*/
+void removeReqDuplicada() {
+  int req1BlocoFinal;
+  int req2BlocoFinal;
+
+  for(int i = 0; i < qntReq; i++) {
+    req1BlocoFinal = req[i].blocoInicial + req[i].numDeBlocos;
+    for(int j = 0; j < qntReq; j++) {
+      req2BlocoFinal = req[j].blocoInicial + req[j].numDeBlocos;
+
+      if(req[i].operacao != req[j].operacao || i == j || req[i].operacao == 'w' ||
+         req[j].operacao == 'w')
+        continue;
+
+      if(req[i].blocoInicial <= req[j].blocoInicial && req1BlocoFinal >= req2BlocoFinal) {
+        int k;
+        for(k = j; k < qntReq - 1; k++) {
+          req[k] = req[k + 1];
+        }
+        req = realloc(req, sizeof(Requisicao) * --qntReq);
+        return;
+      }
+    }
+  }
+
+  // 100 11
+  // 101 10
+}
+
+/*---------------------------------------------------------------*/
 int main() {
   Requisicao in = {0};
-  int qntReq = 0;
-
-  // Requisicao *requisicoes;
-  Requisicao requisicoes[20];
 
   while(in.blocoInicial >= 0) {
     scanf("%d", &in.blocoInicial);
-    if(in.blocoInicial != -1) {
+    if(in.blocoInicial > -1) {
       scanf("%d %c", &in.numDeBlocos, &in.operacao);
-      if(in.operacao != 'r' && in.operacao != 'w') { exit(EXIT_FAILURE); };
-      qntReq = escalonador(in, requisicoes, ++qntReq);
-      if(qntReq == -1) {
-        // free(requisicoes);
-        perror("Erro na realocação de memória");
+      if(in.operacao != 'r' && in.operacao != 'w') {
+        free(req);
         exit(EXIT_FAILURE);
-      }
+      };
+      escalonador(in);
     }
   }
 
+  qsort(req, qntReq, sizeof(Requisicao), compare);
+
   for(int i = 0; i < qntReq; i++) {
-    printf("%d %d %c\n", requisicoes[i].blocoInicial, requisicoes[i].numDeBlocos,
-           requisicoes[i].operacao);
+    printf("%d %d %c\n", req[i].blocoInicial, req[i].numDeBlocos, req[i].operacao);
   }
 
-  // free(requisicoes);
+  free(req);
 }
